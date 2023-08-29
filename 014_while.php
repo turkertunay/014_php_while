5b607d5184af577ea5b4499ab9c1902948897089
turkertunay <75833839+turkertunay@users.noreply.github.com>
2023-08-29 19:31:02 +0300

    Add files via upload

---
+++ 014_while.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  <?php
+      $index =1;
+      while($index <= 5){
+          echo "$index <br>";
+          $index++;
+      }
+      $indexx = 10;
+      do{
+        echo "$indexx <br>";
+        $indexx++;
+      }while ($indexx<=17);
+
+  ?>
+  </body>
+</html>
